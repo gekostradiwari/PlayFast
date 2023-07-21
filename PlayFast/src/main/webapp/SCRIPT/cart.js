73bcf6695fbb13aea7f554a1732424f6713557e6
fguer69 <119618534+fguer69@users.noreply.github.com>
2023-07-21 10:09:13 +0200

    Update cart.js

--- PlayFast/src/main/webapp/SCRIPT/cart.js
+++ PlayFast/src/main/webapp/SCRIPT/cart.js
@@ -5,17 +5,4 @@
     }
     document.querySelector('.btn').addEventListener('click', goToCheckoutPage);
 
-// Definisci una funzione per gestire il click sul pulsante "Login"
-    function goToSignInPage() {
-        location.reload();
-        window.location.href = 'sing-in.html';
-    }
-    document.querySelector('.dropdown-item[data-bs-theme-value="dark"]').addEventListener('click', goToSignInPage);
-
-// Definisci una funzione per gestire il click sul pulsante "Sign up"
-    function goToSignUpPage() {
-        location.reload();
-        window.location.href = 'sign-up.html';
-    }
-    document.querySelector('.dropdown-item[data-bs-theme-value="dark"]').addEventListener('click', goToSignUpPage);
 
